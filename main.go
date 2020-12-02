23bffc1677960abffe3ab69d4ddf2d043367c7bd
pascualsu <pascual.soto@redsalud.cl>
2020-12-02 13:26:32 -0300

    actualización para que lea y transforme la data desde el archivo .csv

--- main.go
+++ main.go
@@ -1,27 +1,53 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 
 	"gonum.org/v1/gonum/stat"
 )
 
 func main() {
-	var x []float64 = []float64{1.0, 2.35, 1.82, 2.22, 4.0}
-	// var x []float64 = []float64{4.0, 1.7021, 2.1978, 1.8018, 1.0}
-	var y []float64 = []float64{0.33, 0.25, 0.162, 0.118, 0.001}
+	// Abrimos el archivo data.csv
+	file, err := os.Open("data.csv")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer file.Close()
+
+	// Generamos un reader
+	reader := csv.NewReader(file)
+	lines, err := reader.ReadAll()
+	if err == io.EOF {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	X := make([]float64, len(lines)-1)    // Vectior X -> rating madurez BIM
+	Y := make([]float64, len(lines)-1)    // Vectir Y -> desviación porcentual de costos
+	Xind := make([]float64, len(lines)-1) // Vectir Xind -> indicador de madurez BIM propuesto
 	var weights []float64
 	var origin bool = false
 
-	var xs = make([]float64, len(x))
-	for i := range xs {
-		xs[i] = 4 / x[i]
+	for i, line := range lines[1:] {
+		X[i], _ = strconv.ParseFloat(line[1], 64)
+		Y[i], _ = strconv.ParseFloat(line[0], 64)
+		Xind[i] = 4 / X[i]
 	}
 
-	alpha, beta := stat.LinearRegression(xs, y, weights, origin)
-	r2 := stat.RSquared(xs, y, weights, alpha, beta)
+	alpha, beta := stat.LinearRegression(Xind, Y, weights, origin)
+	r2 := stat.RSquared(Xind, Y, weights, alpha, beta)
 
+	fmt.Println("DATOS GENERALES")
+	fmt.Printf("Vector Madurez BIM:           %.3f\n", X)
+	fmt.Printf("Vector Indicador Madurez BIM: %.3f\n", Xind)
+	fmt.Printf("Vector Desviación de costos:  %.3f\n", Y)
+	fmt.Println("ESTADÍSTICOS")
 	fmt.Printf("Constante:  %.4f\n", alpha)
 	fmt.Printf("Estimador:   %.4f\n", beta)
-	fmt.Printf("R^2: %.6f\n", r2)
+	fmt.Printf("R^2:         %.4f\n", r2)
 }
